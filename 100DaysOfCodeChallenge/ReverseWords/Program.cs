183913b1ff2cd3a076e9f628e47b1b10adb32a9d
unknown <Bones4pk@gmail.com>
2024-03-14 11:38:38 +0200

    Add new exercise

---
+++ 100DaysOfCodeChallenge/ReverseWords/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ReverseWords
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine()
+                .Split();
+
+            Console.WriteLine(String.Join(" ", words.Reverse()));
+        }
+    }
+}
